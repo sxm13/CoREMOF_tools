ebbe4aa74f8dec81552e7e0cd8d3a12d5b7c5f38
sxm13 <342247760@qq.com>
2025-05-17 09:56:46 +0900

    publish new version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = readme_path.read_text(encoding="utf-8") if readme_path.exists
 
 setup(
     name='CoREMOF_tools',
-    version='0.1.5',
+    version='0.1.6',
     author='Guobin Zhao',
     author_email='sxmzhaogb@gmail.com',
     description='Python API for CoRE MOF DB',
